782fc2d94ad7365f46983912d3e0fcd3184432c0
Chris Lynch <chrislynch42@yahoo.com>
2015-12-29 09:52:41 -0500

    Attachment are JBake assets

--- src/test/java/com/codeexcursion/AttachmentPathBuilderTest.java
+++ src/test/java/com/codeexcursion/AttachmentPathBuilderTest.java
@@ -1,26 +1,9 @@
 package com.codeexcursion;
 
 import com.codeexcursion.path.AttachmentPathBuilder;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
-import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -37,7 +20,7 @@ public class AttachmentPathBuilderTest extends PathCommon {
   @Test 
   public void testGetDirectories() {
     String path = pathBuilder.getDirectories();
-    Assert.assertEquals("Directories did not match.", "./attachment/2012/02", path);    
+    Assert.assertEquals("Directories did not match.", "./assets/2012/02", path);    
   }
   
   @Test 
